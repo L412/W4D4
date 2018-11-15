9a107cadecfe1c073efe42793b7a8d3651347a7e
SHP <skylarprill@gmail.com>
2018-11-15 15:09:17 -0800

    Write all methods in BandsController, write bands views

--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create, :new, :show]
   resources :sessions, only: [:create, :new, :destroy]
+  resources :bands
 
 end
--- music_app/db/migrate/20181115215133_create_bands_table.rb
+++ music_app/db/migrate/20181115215133_create_bands_table.rb
@@ -1,6 +1,9 @@
 class CreateBandsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :bands_tables do |t|
+    create_table :bands do |t|
+      t.string :name, null: false
+      t.timestamps
     end
+    add_index :bands, :name, unique: true
   end
 end
