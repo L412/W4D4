7e9352b249f3d43b49796aa777c734d2860308eb
SHP <skylarprill@gmail.com>
2018-11-15 10:30:02 -0800

    Write methods and validations for User model

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -11,4 +11,45 @@
 #
 
 class User < ApplicationRecord
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password_digest, length: { minimum: 6, allow_nil: true }
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom_urlsafe_base64
+  end
+
+  def self.reset_session_token
+    @session_token = SecureRandom_urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def self.ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def self.password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.is_password?(password)
+    BCrypt::Password.create(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && User.is_password?(password)
+      return user
+    else
+      nil
+    end
+  end
+
 end
